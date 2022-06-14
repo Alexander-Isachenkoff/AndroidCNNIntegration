afbad40a27d3603d5cb5ecd4d01cfda0e6946407
isachenkoff <isachenkoff.alexander@yandex.ru>
2022-06-14 16:30:26 +0800

    .

--- app/src/main/java/ru/isachenkoff/cnnintegration2/ClassifierBuilder.java
+++ app/src/main/java/ru/isachenkoff/cnnintegration2/ClassifierBuilder.java
@@ -2,6 +2,7 @@ package ru.isachenkoff.cnnintegration2;
 
 import android.content.res.AssetFileDescriptor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -12,6 +13,7 @@ import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -40,18 +42,24 @@ public class ClassifierBuilder {
             throw new RuntimeException("Не удалось считать модель");
         }
         
-        Map<String, String> params = getLines(PARAMS_FILE)
-                .map(s -> s.split(": "))
-                .collect(Collectors.toMap(strings -> strings[0], strings -> strings[1]));
+        Map<String, String> params = readParamsMap(PARAMS_FILE);
         int input_width = Integer.parseInt(tryGetParam("INPUT_WIDTH", params));
         int input_height = Integer.parseInt(tryGetParam("INPUT_HEIGHT", params));
         int classes_count = Integer.parseInt(tryGetParam("CLASSES_COUNT", params));
         
-        Map<Integer, String> classesMap = getLines(CLASSES_FILE)
-                .map(s -> s.split(": "))
-                .collect(Collectors.toMap(strings -> Integer.parseInt(strings[0]), strings -> strings[1]));
-        
+        Map<Integer, String> classesMap = readParamsMap(CLASSES_FILE, Integer::parseInt);
         return new Classifier(model, input_width, input_height, classes_count, classesMap);
+    }
+    
+    private Map<String, String> readParamsMap(String file) {
+        return readParamsMap(file, s -> s);
+    }
+    
+    @NonNull
+    private <K> Map<K, String> readParamsMap(String file, Function<String, K> keyFunction) {
+        return getLines(file)
+                .map(s -> s.split(": "))
+                .collect(Collectors.toMap(strings -> keyFunction.apply(strings[0]), strings -> strings[1]));
     }
     
     private ByteBuffer getModel() {
